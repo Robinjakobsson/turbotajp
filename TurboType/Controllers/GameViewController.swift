548ef562b7abb4c8874129ef98a827195e020583
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2025-03-25 10:06:06 +0100

    Added viewcontrollers

---
+++ TurboType/Controllers/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameplayViewController.swift
+//  TurboType
+//
+//  Created by Robin jakobsson on 2025-03-25.
+//
+
+import UIKit
+
+class GameplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
