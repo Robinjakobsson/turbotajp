162381c5a601c77d17413aad25bfa46c85593fcc
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2025-03-25 09:02:56 +0100

    Initial Commit

---
+++ TurboType/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TurboType
+//
+//  Created by Robin jakobsson on 2025-03-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TurboTypeTests/TurboTypeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TurboTypeTests.swift
+//  TurboTypeTests
+//
+//  Created by Robin jakobsson on 2025-03-25.
+//
+
+import Testing
+@testable import TurboType
+
+struct TurboTypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
