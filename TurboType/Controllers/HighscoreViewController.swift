fe6ba40c38195bef73b94cf002226544545d104d
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2025-03-26 09:20:16 +0100

    helloworld

--- TurboType/Controllers/HighscoreViewController.swift
+++ TurboType/Controllers/HighscoreViewController.swift
@@ -9,8 +9,27 @@ import UIKit
 
 class HighscoreViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    
+    
+}
+
+extension HighscoreViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
